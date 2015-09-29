71f1b33d454ed7d0396f285d14ae7a7f959ef3ca
kkosuge <root@kksg.net>
2015-09-30 01:54:58 +0900

    Add refresh button

--- src/renderer/components/article_header.js
+++ src/renderer/components/article_header.js
@@ -1,7 +1,16 @@
+import Timeline from '../timeline/timeline'
+
 export default class ArticleHeader extends React.Component {
+  refresh() {
+    Timeline.currentWindow().update();
+  }
+
   render() {
     return(
      <div className="article-header">
+       <div className="refresh" onClick={ this.refresh }>
+         <i className="fa fa-refresh"></i>
+       </div>
        <h1>{ this.props.windowName }</h1>
        <div className="ui icon input">
          <input type="text" placeholder="Filter..." />
